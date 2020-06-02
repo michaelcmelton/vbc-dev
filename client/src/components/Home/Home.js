db6bc2815c2056ce4f1fcb4852e6a1234da93c6d
michaelcmelton <michaelcmelton93@gmail.com>
2020-06-02 08:41:25 -0400

    feat(client): Implement header Home Component
    - Implement base header for Home Component.

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -9,8 +9,11 @@ const Home = (props) => {
         <div className="home">
            <div className="header">
                <img className="header-logo" alt="logo" src={imgPath + 'VBC.png'} />
-               <h1 className="header-banner">You Served. They Served.</h1>
-               <h4 className="header-subtext">Let us connect you as Civilans.</h4>
+               <div className="header-flex-container">
+                    <h1 className="header-banner">You Served. They Served.</h1>
+                    <h4 className="header-subtext">Let us connect you as Civilans.</h4>
+               </div>
+               <img className="mobile-header-logo" alt="logo" src={imgPath + 'VBC.png'} />
            </div>
            <div>
                <p className="body-text">
@@ -18,6 +21,17 @@ const Home = (props) => {
                Our vision is to bring together military supporters, veterans, and veteran business owners in their communities on a platform that will allow them to connect and achieve mission completion whether it is needing a plumber, 
                cake decorator, mechanic, or financial advisor. We believe the strength of the veteran community lies in the bonds forged in service. We will facilitate that trust and bond in the civilian world.
                </p>
+           </div>
+           <div className="box-container">
+                <div className="box">
+                        
+                </div>
+                <div className="box">
+
+                </div>
+                <div className="box">
+
+                </div>
            </div>
         </div>
     );
