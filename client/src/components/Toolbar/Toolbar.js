d1073c3594ade1a9fece97ec86b41b29371df912
michaelcmelton <michaelcmelton93@gmail.com>
2020-06-05 23:44:54 -0400

    feat(client):Add Login route to VBC staging for testing

--- client/src/components/Toolbar/Toolbar.js
+++ client/src/components/Toolbar/Toolbar.js
@@ -17,6 +17,7 @@ const Toolbar = (props) => {
                 <div className="nav">
                     <ul className="nav-menu">
                         <Link to='/'><button className="nav-menu-item">Home</button></Link>
+                        <Link to='/login'><button className="nav-menu-item">Login</button></Link>
                         <Link to='/profile'><button className="nav-menu-item">Profile</button></Link>
                         <Link to='/directory'><button className="nav-menu-item">Directory</button></Link>
                         <Link to='/contact'><button className="nav-menu-item">Contact</button></Link>
